2f02fc1824862343008d8e683b86e2464760b5db
Pedro Manse <pedro.manse@dmk3.com.br>
2025-04-09 15:27:51 -0300

    main: execution mode + updated comments

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,35 @@ use stt::*;
 
 // TODO error reporting
 
-// TODO execution mode
-// : normal
-// : debug --debug
-// : syntax --raw-syncheck
-// : preproc --syncheck
-
-
 // TODO make (if) kw for common case of (if) {check} {if-code} {true} {else-code}
 
 // TODO * mode for Ifs to non-exclusive execution
 
 // TODO (pragma <command>) // once -> only execute file once // store in execution context
 
+//TODO type checking (fn) [a:num b:string x:array] { ... }
+// : would llow for (fn) [x:string] assert$is-string { x }
+// : would allow for (fn) [arr:array] assert$arr$of-string { (while) ... { assert$is-string } }
+
 pub enum SttMode {
     Normal,
     Debug,
-    Syntax,
+    SyntaxCheck,
+    TokenCheck,
 }
 
 fn main() {
-    let mut args = std::env::args().skip(1);
+    let mut args = std::env::args().skip(1).peekable();
     let file_path = args.next().unwrap();
-    let mode = SttMode::Normal;
+    let mode = args.peek();
+    let mode = match mode.map(|s|s.as_str()) {
+        None => SttMode::Normal,
+        Some("--debug") => SttMode::Debug,
+        Some("--token") => SttMode::TokenCheck,
+        Some("--syntax") => SttMode::SyntaxCheck,
+        Some(x) => panic!("No suck execution mode {x}"),
+    };
+
     use SttMode as M;
     match mode {
         M::Normal => {
@@ -33,8 +39,11 @@ fn main() {
         M::Debug => {
             todo!()
         }
-        M::Syntax => {
+        M::SyntaxCheck => {
             get_project_code(file_path).unwrap();
+        }
+        M::TokenCheck => {
+            get_tokens(file_path).unwrap();
         }
     }
 }
