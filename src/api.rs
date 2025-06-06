e7d2b566c401da55d906e3148c095ad92a043350
Pedro Manse <pedro.manse@dmk3.com.br>
2025-06-06 00:12:21 -0300

    api: add doc comments and have fewer public functions

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,49 @@
+//! # This module exposes the steps of the pipeline for file execution
+//! The steps are:
+//! 1. Parsing and preprocessing the file into tokens, avaliable with [get_tokens]
+//! 2. Pre-processing the file, also avaliable with [get_tokens]
+//! 3. Parsing the tokens into code, avaliable with [get_project_code]
+//! 4. Executing the code, avaliable with [execute_file]
+//!
+//! Each step executes the previous one aswell to forbid jump pipeline steps
+//!
+
 use crate::*;
 
-// step token.rs
-pub fn get_raw_tokens(file_path: &Path) -> Result<TokenBlock> {
+/// # Get tokens by filepath
+/// ```rust
+/// let tokens = stt::api::get_tokens("examples/test.stt");
+/// eprintln!("{:?}", tokens);
+/// ```
+pub fn get_tokens(path: impl AsRef<Path>) -> Result<TokenBlock> {
+    let file_path = PathBuf::from(path.as_ref());
+    let tokens = get_raw_tokens(&file_path)?;
+    preproc_tokens(tokens, &file_path)
+}
+
+/// # Parse tokens into code
+/// ```rust
+/// let code = stt::api::get_project_code("examples/test.stt");
+/// eprintln!("{:?}", code);
+/// ```
+pub fn get_project_code(path: impl AsRef<Path>) -> Result<Code> {
+    let TokenBlock { tokens, source } = get_tokens(path)?;
+    let mut parser = parse::Context::new(tokens);
+    let exprs = parser.parse_block()?;
+    Ok(Code { exprs, source })
+}
+
+/// # Execute code
+/// ```rust
+/// stt::api::execute_file("examples/test.stt");
+/// ```
+pub fn execute_file(path: impl AsRef<Path>) -> Result<()> {
+    let expr_block = get_project_code(path)?;
+    execute_code(expr_block)
+}
+
+// steps for token.rs:
+fn get_raw_tokens(file_path: &Path) -> Result<TokenBlock> {
     let Ok(cont) = std::fs::read_to_string(file_path) else {
         return Err(SttError::CantReadFile(file_path.to_path_buf()));
     };
@@ -12,8 +54,17 @@ pub fn get_raw_tokens(file_path: &Path) -> Result<TokenBlock> {
     })
 }
 
-// step preproc.rs
-pub fn preproc_tokens(
+pub(crate) fn get_tokens_with_procvars(
+    path: impl AsRef<Path>,
+    proc_vars: &mut HashSet<String>,
+) -> Result<TokenBlock> {
+    let file_path = PathBuf::from(path.as_ref());
+    let tokens = get_raw_tokens(&file_path)?;
+    preproc_tokens_with_vars(tokens, &file_path, proc_vars)
+}
+
+// steps for preproc.rs:
+fn preproc_tokens(
     TokenBlock { tokens, source }: TokenBlock,
     file_path: &Path,
 ) -> Result<TokenBlock> {
@@ -23,7 +74,7 @@ pub fn preproc_tokens(
     Ok(TokenBlock { tokens, source })
 }
 
-pub fn preproc_tokens_with_vars(
+fn preproc_tokens_with_vars(
     TokenBlock { tokens, source }: TokenBlock,
     file_path: &Path,
     vars: &mut HashSet<String>,
@@ -34,43 +85,9 @@ pub fn preproc_tokens_with_vars(
     Ok(TokenBlock { tokens, source })
 }
 
-// step parse.rs
-pub fn parse_tokens(TokenBlock { tokens, source }: TokenBlock) -> Result<Code> {
-    let mut parser = parse::Context::new(tokens);
-    let exprs = parser.parse_block()?;
-    Ok(Code { exprs, source })
-}
-
-pub fn execute_code(code: Code) -> Result<()> {
+// step for runtime:
+fn execute_code(code: Code) -> Result<()> {
     let mut executioner = runtime::Context::new();
     executioner.execute_code(&code.exprs, &code.source)?;
     Ok(())
-}
-
-// abstract
-pub fn get_tokens(path: impl AsRef<Path>) -> Result<TokenBlock> {
-    let file_path = PathBuf::from(path.as_ref());
-    let tokens = get_raw_tokens(&file_path)?;
-    preproc_tokens(tokens, &file_path)
-}
-
-pub fn get_tokens_with_procvars(
-    path: impl AsRef<Path>,
-    proc_vars: &mut HashSet<String>,
-) -> Result<TokenBlock> {
-    let file_path = PathBuf::from(path.as_ref());
-    let tokens = get_raw_tokens(&file_path)?;
-    preproc_tokens_with_vars(tokens, &file_path, proc_vars)
-}
-
-pub fn get_project_code(path: impl AsRef<Path>) -> Result<Code> {
-    let TokenBlock { tokens, source } = get_tokens(path)?;
-    let mut parser = parse::Context::new(tokens);
-    let exprs = parser.parse_block()?;
-    Ok(Code { exprs, source })
-}
-
-pub fn execute_file(path: impl AsRef<Path>) -> Result<()> {
-    let expr_block = get_project_code(path)?;
-    execute_code(expr_block)
 }
