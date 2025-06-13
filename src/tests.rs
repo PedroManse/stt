629ac9f27dc86a8f5e3c2933b7ba26905230d14b
Pedro Manse <pedro.manse@dmk3.com.br>
2025-06-13 12:36:50 -0300

    test.ts: removed StrVec -> StringVec trait, no longer needed

--- src/tests.rs
+++ src/tests.rs
@@ -3,16 +3,6 @@ mod runtime;
 mod token;
 mod typing;
 
-trait StrVecIntoStringVec {
-    fn into_strings(self) -> Vec<String>;
-}
-
-impl StrVecIntoStringVec for Vec<&str> {
-    fn into_strings(self) -> Vec<String> {
-        self.into_iter().map(String::from).collect()
-    }
-}
-
 // like assert_eq but shows `got` and `expected`
 macro_rules! test_eq {
     (got: $got:expr, expected: $expected:expr) => {{
